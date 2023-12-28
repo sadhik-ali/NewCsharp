8fb5201e9786de17d2a9a74c9a58c5a6acdb3782
sadhikaliR <sadhikali0867@gmail.com>
2023-12-27 18:24:52 -0800

    first

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// Console.WriteLine(10+10);
+
+
+// Relational
+
+// int a = 10;
+// int b = 30;
+
+// Console.WriteLine(a<b);
+// Console.WriteLine(a<=b);
+// Console.WriteLine(a>b);
+// Console.WriteLine(a>=b);
+// Console.WriteLine(a==b);
+// Console.WriteLine(a!=b);
+
+// Logical 
+
+// int x = 10;
+// int y = 5;
+
+// Console.WriteLine(x == 10 && y == 5);
+// Console.WriteLine(x == 10 || y == 5);
+// Console.WriteLine(!(x == 10));
+ 
+// Assignment
+
+// int x = 20;
+//  x += 10; // x = x+10
+//  x -= 10; // x = x-10
+//  x *= 10; // x = x*10
+//  x /= 10; // x = x/10
+//  x %= 10; // x = x%10
+
+// Console.WriteLine(x);
+
+
+// Unary
+// int a = 10;
+// a++; //a=a+1;
+// a--; //a=a-1;
+// Console.WriteLine(a);
+
+// Ternary
+
+// int x = 10;
+
+// String result = x > 0 ? "positive":"negative";
+
+// Console.WriteLine(result);
